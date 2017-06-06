4091ab1b7e8c030a3432ae6d9e61c5328e1a1a35
hongyang <hongyooo@icloud.com>
2017-06-06 19:45:29 +0800

    versoin 1.0

---
+++ app/src/main/java/example/rxpermission/MainActivity.java
@@ -0,0 +1,28 @@
+package example.rxpermission;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+import rx.permission.RxKtPermission;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RxKtPermission rxKtPermission = new RxKtPermission(this);
+        rxKtPermission.requestCamera().subscribe(new Consumer<Boolean>() {
+            @Override
+            public void accept(@io.reactivex.annotations.NonNull Boolean aBoolean) throws Exception {
+
+            }
+        });
+    }
+}
